43841e9fa0df6fa06eb5030a7fa97b9d8f88af8e
Byabasaija <basaijapascal9@gmail.com>
2021-04-07 13:49:33 +0300

    finish the functionality of the weather app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,77 @@
+// display users current location weather by default
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+
+
+const api = 'a09b4085ef795cb894f486def4422bec';
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Get the Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+
+      fetch(base)
+        .then((response) => response.json())
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+
+
+          const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+          const fahrenheit = (temp * 9) / 5 + 32;
+
+
+          iconImg.src = iconUrl;
+          loc.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          tempC.textContent = `${temp.toFixed(2)} °C`;
+          tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+        });
+    });
+  }
+});
 
 const form = document.querySelector('.top-banner form');
+const input = document.querySelector('.top-banner input');
+const msg = document.querySelector('.top-banner .msg');
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-});
+  const inputValue = input.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${api}&units=metric`;
 
-const api = 'a09b4085ef795cb894f486def4422bec';
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { temp } = data.main;
+      const place = data.name;
+      const { description, icon } = data.weather[0];
+
+      const iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      const fahrenheit = (temp * 9) / 5 + 32;
+
+
+      loc.textContent = `${place}`;
+      iconImg.src = iconUrl;
+      desc.textContent = `${description}`;
+      tempC.textContent = `${temp.toFixed(2)} °C`;
+      tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+    })
+    .catch(() => {
+      msg.textContent = ' There is no such a place 😩';
+    });
+
+  msg.textContent = '';
+  form.reset();
+  input.focus();
+});
